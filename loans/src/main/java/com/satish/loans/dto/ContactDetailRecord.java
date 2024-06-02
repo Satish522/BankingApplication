40ff52492729a53f6709f14351280734f72f655b
Satish522 <satishsubudhi522@gmail.com>
2024-06-02 16:41:13 +0530

    config from from repository to refresh

--- loans/src/main/java/com/satish/loans/dto/ContactDetailRecord.java
+++ loans/src/main/java/com/satish/loans/dto/ContactDetailRecord.java
@@ -1,5 +1,7 @@
 package com.satish.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -14,6 +16,12 @@ import java.util.Map;
  * @author satishkumarsubudhi
  * @since 01/06/24
  */
+@Getter
+@Setter
 @ConfigurationProperties("loans")
-public record ContactDetailRecord(String message, Map<String, String> contactDetails, List<String> callSupport) {
+public class ContactDetailRecord {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> callSupport;
 }
